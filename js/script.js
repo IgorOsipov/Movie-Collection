6814823b01993cc5945c9d589ff68f3ddb48f3e1
IgorOsipov <awesomeigor@gmail.com>
2020-08-28 18:28:59 +0300

    made an adaptive layout

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
+    const menuButton = $('#menu_button')
+    const blackFill = $('#black_fill')
+    const mainMenu = $('.main_menu')
+    menuButton.click(function(){
+        $('.menu_box').show()
+        blackFill.show()
+    })
+    blackFill.click(function(){
+        blackFill.hide()
+        $('.menu_box').hide()
+    })
+    mainMenu.click(function(){
+        blackFill.hide()
+        $('.menu_box').hide()
+    })
+
     $('#slider').slick({
         arrows: true,
         dots: false,
         autoplay: true,
-        autoplaySpeed: 10000
+        autoplaySpeed: 10000,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false               
+              }
+            }
+        ]
     })
 })
